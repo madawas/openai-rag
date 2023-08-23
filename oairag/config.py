b04859bbd78e2058f9714ded81f665c8ccb7c45c
Madawa Soysa <madawa.rc@gmail.com>
2023-08-23 21:00:51 +1000

    Refactor source

--- oairag/config.py
+++ oairag/config.py
@@ -1,12 +1,47 @@
-from pydantic import DirectoryPath, Field, PostgresDsn
+from pydantic import BaseModel
+from pydantic import DirectoryPath, Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', case_sensitive=False)
+    """
+    Settings class to define configuration parameters.
+
+    Attributes:
+        model_config (SettingsConfigDict): Configuration dictionary for model settings.
+        doc_upload_dir (DirectoryPath): Directory path for document uploads.
+
+    """
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
     doc_upload_dir: DirectoryPath = Field(default='/Users/madawa/projects/openai-rag/test')
 
 
 settings = Settings()
 
-print(settings.model_dump())
+
+class SuccessResponse(BaseModel):
+    """
+    Success response model.
+
+    Attributes:
+        status (str): Status of the response ('success').
+        message (str): Message indicating the response status.
+
+    """
+    status: str = 'success'
+    message: str
+
+
+class ErrorResponse(BaseModel):
+    """
+    Error response model.
+
+    Attributes:
+        status (str): Status of the response ('error').
+        message (str): Message indicating the response status.
+        exception (str): String representing the exception details.
+
+    """
+    status: str = 'error'
+    message: str
+    exception: str
--- oairag/routers/documents.py
+++ oairag/routers/documents.py
@@ -1,8 +1,10 @@
 import os
+from typing import Union
+
 import aiofiles
-from fastapi import APIRouter, File, UploadFile
-from fastapi.responses import JSONResponse
-from ..config import settings
+from fastapi import APIRouter, File, UploadFile, status, Response
+
+from ..config import ErrorResponse, SuccessResponse, settings
 
 router = APIRouter(
     prefix='/documents',
@@ -11,32 +13,47 @@ router = APIRouter(
 
 
 @router.post(
-    path='/upload'
+    path='/upload',
+    summary='Upload a document to process',
+    description='Upload a document to extract the text, chunk it and embed the chunks and store '
+                'in a vector store',
+    status_code=status.HTTP_201_CREATED,
+    responses={500: {'model': ErrorResponse, 'description': 'Server Error'}}
 )
-async def doc_upload(file: UploadFile = File(...)) -> JSONResponse:
-    print(settings.model_dump())
+async def doc_upload(response: Response, file: UploadFile = File(...)) -> \
+        Union[SuccessResponse, ErrorResponse]:
+    """
+    Uploads a text document to be processed.
+
+    Args:
+        response (Response): The FastAPI Response object to modify in case of errors.
+        file (UploadFile): The UploadFile object representing the uploaded file.
+
+    Returns:
+        Union[ErrorResponse, SuccessResponse]: Returns an ErrorResponse object if an error occurs,
+            otherwise returns a SuccessResponse object indicating successful file upload.
+
+    This function uploads a document file by reading the contents of the provided UploadFile
+    object and saving it to the directory specified in the settings. The file is read in chunks
+    of 1MB and asynchronously written to the file path. Any exceptions that occur during the upload
+    process are caught and handled by modifying the response object's status code and returning an
+    ErrorResponse. If the upload is successful, a SuccessResponse is returned.
+
+    Note:
+        Make sure to configure the `settings.model_dump()` and `settings.doc_upload_dir`
+        appropriately.
+
+    """
     file_path = os.path.join(settings.doc_upload_dir, file.filename)
-    print(file_path)
 
     try:
         async with aiofiles.open(file_path, 'wb') as f:
             while contents := await file.read(1024 * 1024):
                 await f.write(contents)
     except Exception as error:
-        return JSONResponse(
-            content={
-                'status': 'error',
-                'message': f'Error occurred while uploading {file.filename}',
-                'exception': str(error)
-            },
-            status_code=500
-        )
+        response.status_code = 500
+        return ErrorResponse(message=f'Error occurred while uploading {file.filename}',
+                             exception=repr(error))
     finally:
         await file.close()
-    return JSONResponse(
-        content={
-            'status': 'success',
-            'message': f'File {file.filename} uploaded successfully'
-        },
-        status_code=201
-    )
+    return SuccessResponse(message=f'File {file.filename} uploaded successfully')
