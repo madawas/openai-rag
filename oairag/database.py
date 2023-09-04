214db92ae1edeeac7190c06aaedf8293fe25d61a
Madawa Soysa <madawa.rc@gmail.com>
2023-09-04 22:54:23 +1000

    Add doc strings to the database module

--- oairag/database.py
+++ oairag/database.py
@@ -26,7 +26,12 @@ _pg_session = async_sessionmaker(
 )
 
 
-async def get_db_session():
+async def get_db_session() -> AsyncSession:
+    """
+    Get an asynchronous database session.
+
+    :return: AsyncSession: A database session.
+    """
     db_session = _pg_session()
     try:
         yield db_session
@@ -37,6 +42,14 @@ async def get_db_session():
 def get_vector_store(
     embedding_function, collection_name=settings.default_collection
 ) -> PGVector:
+    """
+    Get a PGVector store.
+
+    :param embedding_function: The embedding function.
+    :param collection_name: The name of the collection. Defaults to settings.default_collection.
+
+    :return: PGVector: A PGVector store.
+    """
     vector_store = PGVector(
         connection_string=f"postgresql+psycopg2://{settings.postgres_user}:"
         f"{settings.postgres_password}@{settings.postgres_host}"
@@ -50,10 +63,18 @@ def get_vector_store(
 
 
 class Base(DeclarativeBase):
+    """
+    Base class for SQLAlchemy declarative models.
+    """
+
     type_annotation_map = {dict: JSON, list[float]: Vector}
 
 
 class DocumentRecord(Base):
+    """
+    Represents a document record in the database.
+    """
+
     __tablename__ = "document"
 
     __mapper_args__ = {"eager_defaults": True}
@@ -72,6 +93,10 @@ class DocumentRecord(Base):
 
 
 class Collection(Base):
+    """
+    Represents a collection in the database.
+    """
+
     __tablename__ = "langchain_pg_collection"
     uuid: Mapped[UUID4] = mapped_column(primary_key=True)
     name: Mapped[str]
@@ -82,6 +107,10 @@ class Collection(Base):
 
 
 class Embedding(Base):
+    """
+    Represents an embedding in the database.
+    """
+
     __tablename__ = "langchain_pg_embedding"
     uuid: Mapped[UUID4] = mapped_column(primary_key=True)
     collection_id: Mapped[UUID4] = mapped_column(
@@ -96,10 +125,26 @@ class Embedding(Base):
 
 
 class DocumentDAO(object):
+    """
+    Data Access Object (DAO) for managing document records.
+    """
+
     def __init__(self, session: AsyncSession):
+        """
+        Initialize the DocumentDAO.
+
+        :param session: An asynchronous database session.
+        """
         self.session = session
 
     async def add_document(self, document: DocumentResponse) -> DocumentRecord:
+        """
+        Add a document to the database.
+
+        :param document: The document to add.
+
+        :return: DocumentRecord: The added document record.
+        """
         doc_entry = DocumentRecord(
             file_name=document.file_name,
             process_status=document.process_status,
@@ -115,16 +160,36 @@ class DocumentDAO(object):
     async def get_documents(
         self, page: int = 0, size: int = 20
     ) -> Sequence[DocumentRecord]:
+        """
+        Get a list of documents from the database.
+
+        :param page: The page number. Defaults to 0.
+        :param size: The number of documents per page. Defaults to 20.
+
+        :return: Sequence[DocumentRecord]: A list of document records.
+        """
         records = await self.session.execute(
             select(DocumentRecord).offset(page * size).limit(size)
         )
         return records.scalars().all()
 
     async def get_document_count(self) -> int:
+        """
+        Get the total count of documents in the database.
+
+        :return: int: The document count.
+        """
         result = await self.session.execute(func.count(DocumentRecord.id))
         return result.scalar()
 
     async def get_document_by_filename(self, filename: str) -> DocumentRecord | None:
+        """
+        Get a document record by filename.
+
+        :param filename: The filename of the document.
+
+        :return: DocumentRecord | None: The document record if found, else None.
+        """
         records = await self.session.execute(
             select(DocumentRecord).where(DocumentRecord.file_name == filename)
         )
@@ -132,12 +197,26 @@ class DocumentDAO(object):
         return records.scalar_one_or_none()
 
     async def get_document_by_id(self, document_id: str) -> DocumentRecord | None:
+        """
+        Get a document record by ID.
+
+        :param document_id: The ID of the document.
+
+        :return: DocumentRecord | None: The document record if found, else None.
+        """
         records = await self.session.execute(
             select(DocumentRecord).where(DocumentRecord.id == document_id)
         )
         return records.scalars().one_or_none()
 
     async def update_document(self, document: DocumentWithMetadata) -> DocumentRecord:
+        """
+        Update a document record.
+
+        :param document: The updated document.
+
+        :return: DocumentRecord: The updated document record.
+        """
         if document.file_name is None:
             raise ValueError("File name is empty")
 
@@ -168,6 +247,11 @@ class DocumentDAO(object):
         return existing_document
 
     async def delete_document(self, document_id):
+        """
+        Delete a document record by ID.
+
+        :param document_id: The ID of the document.
+        """
         LOG.debug("Deleting document with id: %s", document_id)
         await self.session.execute(
             delete(DocumentRecord).where(DocumentRecord.id == document_id)
